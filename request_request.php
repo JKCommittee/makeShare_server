c0e6161fd8ed1d5501ef817e75e9a1c31f3b2686
skht777 <skht77@gmail.com>
2015-12-12 05:34:47 +0900

    記述ミスが有ったので修正

--- request_request.php
+++ request_request.php
@@ -1,9 +1,9 @@
-include_once dirname(__FILE__)."/Post.php";
+include_once dirname(__FILE__)."/Request.php";
 
 $requestDao = new Request();
 
 if ($_POST['func'] === 'request') {
 	$data = $_POST;
 	unset($data['func']);
-	echo json_encode(array('result' => $requestDao->saveRequest($data,)));
+	echo json_encode(array('result' => $requestDao->saveRequest($data)));
 }
