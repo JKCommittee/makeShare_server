c979bd708e64b5257bf5a737ad684816c6a26ed5
skht777 <skht77@gmail.com>
2015-12-11 23:51:07 +0900

    ちょっとだけ直した

--- request_request.php
+++ request_request.php
@@ -4,5 +4,6 @@ $requestDao = new Request();
 
 if ($_POST['func'] === 'request') {
 	$data = $_POST;
+	unset($data['func']);
 	echo json_encode(array('result' => $requestDao->saveRequest($data,)));
 }
