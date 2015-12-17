d256bbe8a7c3ec47607f7dd9407107402a51537f
skht777 <skht77@gmail.com>
2015-12-17 17:59:05 +0900

    やむを得ない事情によりGETリクエスト仕様に変更

--- user_request.php
+++ user_request.php
@@ -3,8 +3,8 @@ include_once dirname(__FILE__)."/User.php";
 
 $userDao = new User();
 
-if ($_POST['func'] === 'user') {
-	$data = $_POST;
+if ($_GET['func'] === 'user') {
+	$data = $_GET
 	unset($data['func']);
 	echo json_encode($userDao->getProfile($data['id']) + $userDao->getRelation($data['id']));
 }
